e28f9bd6c9d9207abd4db2d0aebdeaa47bcb1ff4
joopitz <46931073+San0ix@users.noreply.github.com>
2023-01-02 13:01:00 +0100

    chore(qtile): remove redundant startup script

--- qtile/autostart_x11.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-fish ~/.config/qtile/scripts/autorandr_cycle.fish
-# clipboard
-clipit &
-
-element-desktop &
-feh --bg-fill ~/Pictures/Wallpaper/ultrawide.jpg --bg-fill ~/Pictures/Wallpaper/normal.png
